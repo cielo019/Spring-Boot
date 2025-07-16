0244fadb5ca42ea424ea5fd97f192572fcd332b0
cielo019 <e21019@eng.pdn.ac.lk>
2025-07-16 16:50:07 +0530

    Add data to DB

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+
+            Student mariam = new Student(
+                    "mariam",
+                    23,
+                    LocalDate.of(2000, Month.JANUARY, 12),
+                    "1234@email.com"
+            );
+            Student alex = new Student(
+                    "Alex",
+                    23,
+                    LocalDate.of(2001, Month.JANUARY, 12),
+                    "12al@email.com"
+            );
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+
+        };
+    }
+}
