1c3bd306471b06b868f019446d3b8a2d5d3c366a
cielo019 <e21019@eng.pdn.ac.lk>
2025-07-16 16:36:26 +0530

    Made studentRepository.java file

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -7,16 +8,18 @@ import java.time.LocalDate;
 import java.util.List;
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "marium",
-                        23,
-                        LocalDate.of(2000, 2, 12),
-                        "1234@email.com"
-                )
-        );
+        return studentRepository.findAll();
+
     }
 
 }
